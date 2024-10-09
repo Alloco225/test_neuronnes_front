51cb1bf0d6da71c60422dcd54e6c4e8c81fa5448
Alloco + Ketchup <amanehosanna@gmail.com>
2024-10-09 16:16:20 +0000

    🚧 Work in Progress:  💄 UI: pimping  login page

--- const/app_messages.ts
+++ const/app_messages.ts
@@ -1,9 +1,17 @@
 const APP_MESSAGES = {
+    app_name: "Neuronnes Diary",
     auth: {
         failed: "Email ou mot de passe incorrect",
     },
     unauthenticated: "Vous êtes déconnecté",
     welcomeBack: "Bon retour",
+    login: "Connexion",
+    register: "Inscription",
+    password: "Mot de passe",
+    email: "Addresse email",
+    login_cta: "Entrez votre addresse email pour vous connecter",
+    submit_login: "Se connecter",
+    submit_register: "S'inscrire",
 
 }
 
